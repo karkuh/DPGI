8944c4e3df0e12c5744f7cc7b45cee3576ef2842
karkuh <karkuhkarkuh13@gmail.com>
2024-04-08 00:51:27 +0300

    Created data access class

---
+++ DPGI/Lab4/Data/AdoAssistant.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public class AdoAssistant
+{
+    private string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+    public DataTable TableLoad()
+    {
+        DataTable dt = new DataTable();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT * FROM books";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(dt);
+            connection.Close();
+        }
+
+        return dt;
+    }
+
+    public void InsertBook(string ISBN, string name, string author, string publisher, string year)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = $"INSERT INTO books (ISBN, name, author, publisher, year) VALUES ('{ISBN}', '{name}', '{author}', '{publisher}', {year})";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+
+
+
+
+    public void UpdateBook(string ISBN, string name, string author, string publisher, string year)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = $"UPDATE books SET name = '{name}', author = '{author}', publisher = '{publisher}', year = {year} WHERE ISBN = '{ISBN}'";
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+
+
+    public void DeleteBook(string ISBN)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "DELETE FROM books WHERE ISBN = @ISBN";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ISBN", ISBN);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+}

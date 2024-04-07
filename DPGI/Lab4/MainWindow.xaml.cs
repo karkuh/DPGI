a403d5085c76174fdfe6f6e3f22bd226627c8d31
karkuh <karkuhkarkuh13@gmail.com>
2024-04-08 00:51:08 +0300

    Added MainWindow in lab4

--- DPGI/Lab4/MainWindow.xaml.cs
+++ DPGI/Lab4/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data.SqlClient;
+using Lab4.Enum;
 
 namespace Lab4
 {
@@ -20,9 +12,85 @@ namespace Lab4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AdoAssistant myTable = new AdoAssistant();
+            list.SelectedIndex = 0;
+            list.Focus();
+            list.DataContext = myTable.TableLoad();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeDataWindow changeDataWindow = new ChangeDataWindow(TypeOfAction.Create, null);
+            changeDataWindow.Show();
+            Hide();
+        }
+
+        private DataTable? takeData()
+        {
+            string ISBN = Microsoft.VisualBasic.Interaction.InputBox("Введіть ISBN:");
+            if (ISBN == "")
+            {
+                return null;
+            }
+
+            DataTable dt = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = $"select * froM books where ISBN = {ISBN};";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                else
+                {
+                    MessageBox.Show("Не існує такого ISBN");
+                    return null;
+                }
+
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dt = takeData();
+            if (dt == null) return;
+
+            var changeDataWindow = new ChangeDataWindow(TypeOfAction.Update, dt);
+            changeDataWindow.Show();
+            Hide();
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dt = takeData();
+            if (dt == null) return;
+
+            var changeDataWindow = new ChangeDataWindow(TypeOfAction.Delete, dt);
+            changeDataWindow.Show();
+            Hide();
+        }
+
+        private void MainWindow_OnClosing(object? sender, EventArgs e)
+        {
+            Close();
         }
     }
 }

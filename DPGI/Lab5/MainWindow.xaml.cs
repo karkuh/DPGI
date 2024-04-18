360d2f89f00052892bbbb5a314e6e261fc967920
karkuh <karkuhkarkuh13@gmail.com>
2024-04-18 17:12:07 +0300

    Fix bugs

--- DPGI/Lab5/MainWindow.xaml.cs
+++ DPGI/Lab5/MainWindow.xaml.cs
@@ -25,47 +25,10 @@ namespace Lab5
             _publishers = _context.Publishers.ToList();
             dataGrid1.ItemsSource = _books;
             dataGrid2.ItemsSource = _publishers;
-            JoinTables();
-            var column = dataGrid1.Columns.SingleOrDefault(c => c.Header.ToString() == "PublisherCodeNavigation");
-            if (column != null)
-            {
-                column.Visibility = Visibility.Collapsed;
-            }
-        }
 
-        private void MainWindow_OnClosing(object? sender, CancelEventArgs e)
-        {
-            Close();
+           JoinTables();
         }
-
-        private void Select1Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var books = _context.Books.Where(b => b.PublicationYear == Int32.Parse(publicationYearTextBox.Text))
-                    .ToList();
-                dataGridSelect1.ItemsSource = books;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Помилка під час виконання запиту: " + ex.Message);
-            }
-        }
-
-        private void Select2Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var books = _context.Books.FromSqlRaw("SELECT * FROM Books WHERE ISBN LIKE '%' + {0} + '%'",
-                    publicationISBNTextBox.Text).ToList();
-                dataGridSelect2.ItemsSource = books;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Помилка під час виконання запиту: " + ex.Message);
-            }
-        }
-
+        
         private void JoinTables()
         {
             try
@@ -81,7 +44,7 @@ namespace Lab5
                         book.PublisherCode,
                         publisher.PublisherName
                     };
-
+        
                 dataGridJoin.ItemsSource = query.ToList();
             }
             catch (Exception ex)
@@ -89,5 +52,40 @@ namespace Lab5
                 MessageBox.Show("Помилка під час виконання запиту: " + ex.Message);
             }
         }
+
+
+        private void SelectByYearButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var books = _context.Books.Where(b => b.PublicationYear == Int32.Parse(publicationYearTextBox.Text))
+                    .ToList();
+                dataGridSelect1.ItemsSource = books;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка під час виконання запиту: " + ex.Message);
+            }
+        }
+        
+        private void SelectByISBNButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var books = _context.Books.FromSqlRaw("SELECT * FROM Books WHERE ISBN LIKE '%' + {0} + '%'",
+                    publicationISBNTextBox.Text).ToList();
+                dataGridSelect2.ItemsSource = books;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка під час виконання запиту: " + ex.Message);
+            }
+        }
+        
+        private void MainWindow_OnClosing(object? sender, CancelEventArgs e)
+        {
+            Close();
+        }
+
     }
 }
